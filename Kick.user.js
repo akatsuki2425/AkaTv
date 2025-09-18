8c047bd22d26280c4c704b3dae88ec7a5e8cf224
akatsuki2425 <44656486+akatsuki2425@users.noreply.github.com>
2025-09-18 17:01:24 +0100

    Update Kick.user.js

--- Kick.user.js
+++ Kick.user.js
@@ -8,8 +8,8 @@
 // @grant        none
 // @license      MIT
 // @icon         https://www.futbin.com/favicon.ico
-// @downloadURL  https://raw.githubusercontent.com/akatsuki2425/AkaTv/refs/heads/master/futbin-analyzer.user.js
-// @updateURL    https://raw.githubusercontent.com/akatsuki2425/AkaTv/refs/heads/master/futbin-analyzer.user.js
+// @downloadURL  https://raw.githubusercontent.com/akatsuki2425/AkaTv/refs/heads/master/Kick.user.js
+// @updateURL    https://raw.githubusercontent.com/akatsuki2425/AkaTv/refs/heads/master/Kick.user.js
 // ==/UserScript==
 
 (function () {
